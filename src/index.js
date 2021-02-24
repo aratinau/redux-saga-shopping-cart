32412011f3040d9c6d27ba9b07e45715aebf5fb5
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2021-02-24 21:44:50 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './components/App'
+
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from './reducers'
+import rootSaga from './sagas'
+
+// https://github.com/redux-saga/redux-saga/tree/master/examples/shopping-cart
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
